decb71dc313f801bfe8c806fddabbd86c7933f73
dongryeol <dongyeol01@naver.com>
2022-12-22 15:59:15 +0900

    챌린지 푸는 중

---
+++ challenges/Without.ts
@@ -0,0 +1,50 @@
+/*
+  5117 - Without
+  -------
+  by Pineapple (@Pineapple0919) #medium #union #array
+  
+  ### Question
+  
+  Implement the type version of Lodash.without, Without<T, U> takes an Array T, number or array U and returns an Array without the elements of U.
+  
+  ```ts
+  type Res = Without<[1, 2], 1>; // expected to be [2]
+  type Res1 = Without<[1, 2, 4, 1, 5], [1, 2]>; // expected to be [4, 5]
+  type Res2 = Without<[2, 3, 2, 3, 2, 3, 2, 3], [2, 3]>; // expected to be []
+  ```
+  
+  > View on GitHub: https://tsch.js.org/5117
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Includes<T, U extends any | any[]> = U extends any[]
+  ? U extends [infer A, ...infer Rest]
+    ? T extends A
+      ? true
+      : Includes<T, Rest>
+    : false
+  : T extends U
+  ? true
+  : false;
+type Without<T, U, R extends any[] = []> = T extends [infer A, ...infer Rest]
+  ? Includes<A, U> extends true
+    ? Without<Rest, U, R>
+    : Without<Rest, U, [...R, A]>
+  : R;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<Without<[1, 2], 1>, [2]>>,
+  Expect<Equal<Without<[1, 2, 4, 1, 5], [1, 2]>, [4, 5]>>,
+  Expect<Equal<Without<[2, 3, 2, 3, 2, 3, 2, 3], [2, 3]>, []>>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/5117/answer
+  > View solutions: https://tsch.js.org/5117/solutions
+  > More Challenges: https://tsch.js.org
+*/
